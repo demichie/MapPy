db25f6eefc0e839bf4aee0cc8918a1329e95447b
Mattia de' Michieli Vitturi <demichie@gmail.com>
2023-04-17 16:41:22 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IP=$(/usr/sbin/ipconfig getifaddr en0)
 /opt/X11/bin/xhost + "$IP"
-docker run -it -e DISPLAY="${IP}:0" -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/home/user_sw/TMP test
+docker run -it -e DISPLAY="${IP}:0" -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/home/user_sw/TMP demichie/mappy
 
 
